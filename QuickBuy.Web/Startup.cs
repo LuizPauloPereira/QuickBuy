2c691e6abe1f0880e9ed9e568d4a7ad0da972e0c
Luiz Paulo Pereira <luizpaulopereiraf@gmail.com>
2019-05-19 11:39:26 -0400

    Configurando ProdutoController

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -6,7 +6,9 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using QuickBuy.Dominio.Contratos;
 using QuickBuy.Repositorio.Contexto;
+using QuickBuy.Repositorio.Repositorios;
 
 namespace QuickBuy.Web
 {
@@ -31,6 +33,9 @@ namespace QuickBuy.Web
                                                         option.UseLazyLoadingProxies()
                                                         .UseMySql(connectionString, 
                                                                         m => m.MigrationsAssembly("QuickBuy.Repositorio")));
+            
+            //informando a classe concreta a ser instanciada na "ProdutoController"
+            services.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
